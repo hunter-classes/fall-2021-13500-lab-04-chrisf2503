e6abc32cd28ced09f4cc936dbceaf48c897c9b8a
chrisf2503 <christopher.flores62@myhunter.cuny.edu>
2021-09-27 01:27:36 -0400

    This will declare our function from task A to G to show that they do exist within some cpp file (hence in our functoin file)

---
+++ function.h
@@ -0,0 +1,8 @@
+#pragma once
+void box(int width, int height);
+void checkerboard(int width, int height);
+void cross(int size);
+void lower(int length);
+void upper(int length);
+void trapezoid(int width, int height);
+void checkerboard3x3(int width, int height);
